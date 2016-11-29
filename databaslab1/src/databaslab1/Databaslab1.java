90105ce7b30c259014249b0bb7d50d0c2db63e82
kraken108 <jakob-danielsson@hotmail.com>
2016-11-30 00:19:32 +0100

    pawej

--- databaslab1/src/databaslab1/Databaslab1.java
+++ databaslab1/src/databaslab1/Databaslab1.java
@@ -8,8 +8,15 @@ package databaslab1;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToolBar;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -21,20 +28,47 @@ public class Databaslab1 extends Application {
     
     @Override
     public void start(Stage primaryStage) {
+        
         Button btn = new Button();
-        btn.setText("Say 'Hello World'");
+        btn.setText("Search");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                System.out.println("Hej");
             }
         });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        Button add = new Button();
+        add.setText("Add CD");
         
-        Scene scene = new Scene(root, 300, 250);
+        BorderPane borderPane = new BorderPane();
+     
+        
+        HBox statusbar = new HBox();
+        borderPane.setTop(statusbar);
+        statusbar.setSpacing(10);
+        statusbar.setPadding(new Insets(10, 0, 10, 10));
+        
+        HBox statusbar2 = new HBox();  
+        statusbar2.setSpacing(10);
+        borderPane.setBottom(statusbar2);
+        statusbar2.setPadding(new Insets(10, 0, 10, 10));
+        
+        HBox midbar = new HBox();
+        borderPane.setCenter(midbar);
+        //midbar.setStyle("-fx-background-color: #000000;");
+        
+        TextField txt = new TextField("Enter search word");
+        
+        ChoiceBox<String> choiceBox = new ChoiceBox<>();
+        choiceBox.getItems().addAll("Author", "Title", "Genre", "Rating");
+        choiceBox.setTooltip(new Tooltip("Search by"));
+        choiceBox.getSelectionModel().selectFirst();
+        
+        statusbar2.getChildren().addAll(add);
+        statusbar.getChildren().addAll(btn,txt,choiceBox);
+        Scene scene = new Scene(borderPane, 768, 512);
         
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
