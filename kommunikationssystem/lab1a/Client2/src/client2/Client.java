54f950a9644c0e6b97b3febc0e7415dfd31b03f5
micke093 <michaelhjalmo@gmail.com>
2017-09-15 12:45:03 +0200

    ny client

---
+++ kommunikationssystem/lab1a/Client2/src/client2/Client.java
@@ -0,0 +1,154 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client2;
+
+/**
+ *
+ * @author micke1
+ */
+import java.util.Scanner;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author micke1
+ */
+public class Client {
+
+    private InetAddress ipAddress;
+    private int portNumber;
+    private DatagramSocket ds;
+    private DatagramPacket dp;
+    
+    
+    public Client(String theIPAddress, int portNumber){ 
+            
+          
+        try {
+            ds = new DatagramSocket();
+            ds.setSoTimeout(20000);
+        } catch (SocketException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        this.ds=ds;
+        this.dp=dp;
+        
+        try {
+            this.ipAddress=InetAddress.getByName(theIPAddress);
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        this.portNumber=portNumber;
+        
+        
+    }
+      
+    
+    public int start(){
+      
+        String stringToSend;
+
+        System.out.println("Type HELLO to start");
+        Scanner reader = new Scanner(System.in);
+        stringToSend=reader.nextLine();
+        
+        
+        if(stringToSend.equals("HELLO")){
+            
+            send(stringToSend);
+            
+            if(receive()==-1)
+                return -1;
+            
+            System.out.println("Type START to start");
+            stringToSend=reader.nextLine();
+            if(stringToSend.equals("START")){
+
+                send(stringToSend);
+                
+                if(receive()==-1)
+                    return -1;
+                
+                gameLoop();
+            }
+        }
+        return 0;
+    }
+    
+    private int gameLoop(){
+        
+        Scanner reader = new Scanner(System.in);
+                
+        for(;;){
+         
+            System.out.println("Guess a letter: (GUESS + letter) ");
+            send(reader.nextLine());
+            if(receive()==-1)
+                return -1;                       
+        }        
+    }
+    
+    private void send(String wordToSend){
+        
+        byte[] byteOne = (wordToSend.getBytes());
+        DatagramPacket dpOne = new DatagramPacket(byteOne,byteOne.length,ipAddress,portNumber);
+        
+        try {        
+            ds.send(dpOne);
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private int receive(){
+        
+        byte[] byteTwo = new byte [1024];
+        DatagramPacket dpTwo = new DatagramPacket(byteTwo, byteTwo.length);
+        
+        try {        
+            ds.receive(dpTwo);
+        } catch (Exception e) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, e);
+            ds.close();
+            return -1;
+        } 
+        
+        print(dpTwo);
+        return 0;
+    }
+    
+    private void print(DatagramPacket dp){
+        
+        String str = new String(dp.getData());
+        System.out.println("result is: " + str);
+        
+        if(removeZeros(str).equals("OK"))          
+            System.out.println("Det fungerade");       
+    }
+    
+    private String removeZeros(String str){
+       
+       String s = "";
+       for(int i=0; i<str.length(); i++){
+           if(str.charAt(i) == '\0'){
+               break;
+           }
+           else{
+               s+=str.charAt(i);
+           }           
+       }
+       return s;
+       //test
+   }    
+}
+
