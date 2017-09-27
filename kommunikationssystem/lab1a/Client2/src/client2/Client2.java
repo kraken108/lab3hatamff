93ef5185904f07a49ef08509956d6e5147dcdf19
kraken108 <jakob-danielsson@hotmail.com>
2017-09-27 08:48:16 +0200

    ändringar i lab1a

--- kommunikationssystem/lab1a/Client2/src/client2/Client2.java
+++ kommunikationssystem/lab1a/Client2/src/client2/Client2.java
@@ -9,9 +9,12 @@ package client2;
  *
  * @author micke1
  */
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 /**
  *
@@ -22,22 +25,29 @@ public class Client2 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws Exception{
-        
-        int arg;
-        arg=Integer.parseInt(args[1]);
-        
-        Client c = new Client(args[0], arg);
-        
-        if(c.start()==-1){
-            System.exit(0);
-            System.out.println("KRASH!!!!!!!!");
-        }
-        
+    public static void main(String[] args) throws Exception {
 
-        
+        int arg;
+        arg = Integer.parseInt(args[1]);
+
+        try {
+            Client c = new Client(args[0], arg);
+            if (c.start() == -1) {
+                System.exit(0);
+                System.out.println("KRASH!!!!!!!!");
+            }
+        } catch (SocketException ex) {
+            System.out.println(ex);
+            System.exit(1);
+        }catch(UnknownHostException ex){
+            System.out.println(ex);
+            System.exit(1);
+        }catch(IOException ex){
+            System.out.println(ex);
+            System.exit(1);
+        }
+
         // TODO code application logic here
     }
-    
-}
 
+}
