2e937614927f1646baaaffd0be4a7677dde9c7b7
Michael <michaelhjalmo@gmail.com>
2017-10-11 12:41:46 +0200

    commit all

---
+++ Distinfosystem/dotnetlab2/dotnetlab2/Models/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+/*
+namespace dotnetlab2.Models
+{
+
+    public ApplicationUser Sender { get ; set; }
+    public ApplicationUser Receiver { get ; set; }
+    public Boolean read;
+    public Boolean removed;
+
+    public Message(){
+        read=false;
+        removed=false;
+    }
+
+    ska starta discord från laptopen
+}*/
