c601f5b9b41fb043f1b01822d6cf39d1dd0f2ed7
Michael <michaelhjalmo@gmail.com>
2017-10-12 10:15:12 +0100

    laptop

--- Distinfosystem/dotnetlab2/dotnetlab2/Models/ApplicationUser.cs
+++ Distinfosystem/dotnetlab2/dotnetlab2/Models/ApplicationUser.cs
@@ -9,5 +9,8 @@ namespace dotnetlab2.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string Name { get; set; }
+        public string pwd  { get; set; }
+
     }
 }
---
+++ Distinfosystem/dotnetlab2/dotnetlab2/Models/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetlab2.Models
+{
+    public class Message
+    {
+        public ApplicationUser SourceUser { get; set; }
+        public ApplicationUser DestUSer { get; set; }
+    }
+
+    public ApplicationUser SourceUser;
+    public ApplicationUser DestUser;
+
+
+    
+
+
+}
