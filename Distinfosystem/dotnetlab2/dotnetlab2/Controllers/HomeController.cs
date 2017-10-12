1af12532c18f21370bd1c6bafd85312857161711
Jakob Danielsson <jakob-danielsson@hotmail.com>
2017-10-12 14:05:47 +0200

    ja kanske

--- Distinfosystem/dotnetlab2/dotnetlab2/Controllers/HomeController.cs
+++ Distinfosystem/dotnetlab2/dotnetlab2/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using dotnetlab2.Models.HomeViewModels;
 using dotnetlab2.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace dotnetlab2.Controllers
 {
@@ -56,7 +57,15 @@ namespace dotnetlab2.Controllers
                 throw new ApplicationException($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
 
-            return View(model);
+            var message = new Message();
+            message.Sender = user;
+            message.Msg = model.Message;
+            message.Topic = model.Topic;
+            message.Receiver = await _userManager.FindByIdAsync(model.UserId.ToString());
+            var result = await _context.Messages.AddAsync(message);
+
+            ViewData["Title"] = "blabla";
+            return RedirectToAction(nameof(Index));
         }
 
 
@@ -75,18 +84,62 @@ namespace dotnetlab2.Controllers
         }
 
         [HttpGet]
-        public IActionResult Readpage()
+        public async Task<IActionResult> Readpage()
         {
             ViewData["Message"] = "Your read page.";
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                throw new ApplicationException($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
 
-            return View();
+            var messages = _context.Messages.ToList();
+            List<MessageInfo> messageList = new List<MessageInfo>();
+
+            foreach(Message m in messages)
+            {
+                var mi = new MessageInfo();
+                mi.Message = m.Msg;
+                mi.Topic = m.Topic;
+                mi.SenderName = m.Sender.UserName;
+                mi.ReceiverName = m.Receiver.UserName;
+                messageList.Add(mi);
+            }
+
+            var model = new ReadpageViewModel();
+            model.Messages = messageList;
+
+            return View(model);
         }
 
-        public IActionResult Writepage()
+        [HttpGet]
+        public async Task<IActionResult> Writepage()
         {
-            ViewData["Message"] = "Your write page.";
 
-            return View();
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                throw new ApplicationException($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+
+            var users = _userManager.Users.ToList();
+            List<UserInfo> usernameList = new List<UserInfo>();
+
+            foreach(ApplicationUser au in users)
+            {
+                var u = new UserInfo();
+                u.Username = au.UserName;
+                u.UserId = au.Id;
+                usernameList.Add(u);
+            }
+            var model = new WritepageViewModel();
+            model.UserId = 1;
+            model.Users = usernameList;
+
+            ViewData["Title"] = "Your write page.";
+
+            return View(model);
         }
 
         public IActionResult Error()
---
+++ Distinfosystem/dotnetlab2/dotnetlab2/Models/HomeViewModels/MessageInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetlab2.Models.HomeViewModels
+{
+    public class MessageInfo
+    {
+        public string Topic { get; set; }
+        public string Message { get; set; }
+        public string SenderName { get; set; }
+        public string ReceiverName { get; set; }
+    }
+}
