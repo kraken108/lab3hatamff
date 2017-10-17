68a65195e1b24cc0f062cb3fe5b676559e652f08
Jakob Danielsson <jakob-danielsson@hotmail.com>
2017-10-17 14:54:07 +0200

    23123123

--- Distinfosystem/dotnetlab2/dotnetlab2/Controllers/HomeController.cs
+++ Distinfosystem/dotnetlab2/dotnetlab2/Controllers/HomeController.cs
@@ -92,8 +92,6 @@ namespace dotnetlab2.Controllers
             return View();
         }
 
-
-
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
