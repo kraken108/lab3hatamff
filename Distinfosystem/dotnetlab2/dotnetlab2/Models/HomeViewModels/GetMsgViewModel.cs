f2d99f54b833398b64e016262a9c772fde4e9f43
Jakob Danielsson <jakob-danielsson@hotmail.com>
2017-10-17 10:01:16 +0200

    3rqwrtqetwgr

---
+++ Distinfosystem/dotnetlab2/dotnetlab2/Models/HomeViewModels/GetMsgViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetlab2.Models.HomeViewModels
+{
+    public class GetMsgViewModel
+    {
+        public string Topic { get; set; }
+        public string Msg { get; set; }
+        public DateTime dateTime { get; set; }
+    }
+}
