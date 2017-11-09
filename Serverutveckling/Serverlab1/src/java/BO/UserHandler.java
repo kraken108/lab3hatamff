7f14a2c2de0adf7b1c86090cc1d13d9a3467eec8
Michael <michaelhjalmo@gmail.com>
2017-11-09 14:07:16 +0100

    sdgds

--- Serverutveckling/Serverlab1/src/java/BO/UserHandler.java
+++ Serverutveckling/Serverlab1/src/java/BO/UserHandler.java
@@ -5,26 +5,63 @@
  */
 package BO;
 
+import Model.User;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
 /**
  *
  * @author Jakob
  */
 public class UserHandler {
     
+    private final EntityManager em;
+    private final EntityManagerFactory emf;
     
-    public UserHandler(){}
+    public UserHandler(){
     
-    
+        emf = Persistence.createEntityManagerFactory("Serverlab1PU");
+        em = emf.createEntityManager();
+    }
+        
     public Boolean login(String username, String password){
         //TODO:
         //Check database and return true if user and pw is correct
-        return true;
+        
+        
+        
+        
+        return false;
     }
     
-    public Boolean createUser(String username, String password) throws Exception{
-        //TODO:
-        //Create new user in database if username doesnt already exists.
-        //Throw exception if something happens
-        return true;
+    public void insertUser(String username){
+        Query q = em.createQuery("");
+    }
+    
+    /*
+    public List findWithName(String name) {
+        return em.createQuery(
+        "SELECT DISTINCT user FROM User user WHERE user.username LIKE '" + name + "'");
+        .setParameter("custName", name)
+        .setMaxResults(10)
+        .getSingleResult()).longValue();
+        return null
+    }
+    */
+    
+    
+    public boolean createUser(String username, String password) throws Exception{
+
+            em.getTransaction().begin();
+            User userToInsert= new User(username, password);
+            em.persist(userToInsert);
+            em.flush();
+            em.getTransaction().commit();
+            em.close();
+            emf.close();       
+            
+            return false;
     }
 }
--- Serverutveckling/Serverlab1/src/java/Model/User.java
+++ Serverutveckling/Serverlab1/src/java/Model/User.java
@@ -5,24 +5,73 @@
  */
 package Model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author Jakob
  */
+
+@Entity
+@Table(name = "Users")
 public class User {
     private String username;
+    private Long id;
+    private String password;
     
-    public User(String username){
+    public User(String username, String password){
         this.username = username;
+        this.password = password;
     }
 
+    public User(String username){
+        this.username=username;
+    }
+    
+    public User(){
+        
+    }
+    
+    @Column(name="name")
     public String getUsername() {
         return username;
     }
-
+    
     public void setUsername(String username) {
         this.username = username;
+    }    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id")
+    public Long getId() {
+        return id;
     }
-    
-    
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+    * @return the password
+    */
+    @Column(name="password")
+    public String getPassword() {
+        return password;
+    }
+
+    /*
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
