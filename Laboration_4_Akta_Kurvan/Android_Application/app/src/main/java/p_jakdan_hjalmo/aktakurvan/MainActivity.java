82e16b47b1a693b2949c06690f228cd0837e801a
kraken108 <jakob-danielsson@hotmail.com>
2017-12-19 15:17:37 +0100

    Implementation av googles api för cloud messaging

--- Laboration_4_Akta_Kurvan/Android_Application/app/src/main/java/p_jakdan_hjalmo/aktakurvan/MainActivity.java
+++ Laboration_4_Akta_Kurvan/Android_Application/app/src/main/java/p_jakdan_hjalmo/aktakurvan/MainActivity.java
@@ -1,7 +1,11 @@
 package p_jakdan_hjalmo.aktakurvan;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        
+        checkGoogleApiAvailability();
+
+    }
+
+    private void checkGoogleApiAvailability(){
+        int resultCode = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this.getApplicationContext());
+
+        if(resultCode == 0){
+            Log.i("info","WTF DET FUNKAR JU M8");
+        }else if(resultCode == 1 || resultCode == 2 || resultCode == 3){
+            Log.i("Return code ", "" + resultCode);
+            Dialog d = GoogleApiAvailability.getInstance().getErrorDialog(this,resultCode,resultCode);
+            d.show();
+        }
     }
 }
